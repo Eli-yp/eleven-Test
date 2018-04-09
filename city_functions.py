1188785b2cbbae48c0ba11172f8b6eccb16e29b9
Eli-yp <1094870599@qq.com>
2018-04-09 15:44:47 +0800

    add population

--- city_functions.py
+++ city_functions.py
@@ -1,3 +1,7 @@
-def get_name(city, country):
-	name = city.title() + ', ' + country.title()
+def get_name(city, country, population=''):
+	if population:
+		name = city.title() + ', ' + country.title() + ' - population ' + population
+	else:
+		name = city.title() + ', ' + country.title() 
 	return name
+
--- test_cities.py
+++ test_cities.py
@@ -6,4 +6,8 @@ class NameTestCase(unittest.TestCase):
 		formated_name = get_name('santiago', 'chile')
 		self.assertEqual(formated_name, 'Santiago, Chile')
 
+	def test_city_country_population(self):
+		formated_name = get_name('santiago', 'chile', '500000')
+		self.assertEqual(formated_name, 'Santiago, Chile - population 500000')
+
 unittest.main()
